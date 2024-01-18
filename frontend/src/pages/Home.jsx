61686bd9a8b418e1f4dc764a6ef7c936562abce0
manuel <dankyiemmanuel0@gmail.com>
2024-01-18 00:55:05 +0000

    added assets

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Herosection from '../components/Herosection'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <Herosection/>
+    </div>
   )
 }
 
